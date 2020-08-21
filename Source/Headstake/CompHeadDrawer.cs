0e1a7dcd50ca80f5328c679bd150ab1f1bc32ea7
gguake <54622239+gguake@users.noreply.github.com>
2020-08-21 11:23:00 +0900

    renaming CompHeadAssignable -> CompHeadDrawer, refactoring somethings.

---
+++ Source/Headstake/CompHeadDrawer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace RWBeheading
+{
+    public class CompHeadDrawer : ThingComp
+    {
+        public IHeadDataContainer HeadContainer => (IHeadDataContainer)parent;
+
+        public CompProperties_HeadDrawer Props => (CompProperties_HeadDrawer)props;
+
+        public override void PostExposeData()
+        {
+            Scribe_Values.Look(ref _uniqueRotation, "rotation");
+        }
+
+        public override void Initialize(CompProperties props)
+        {
+            base.Initialize(props);
+
+            _uniqueRotation = Rand.Range(Props.rotationRange.x, Props.rotationRange.z);
+        }
+
+        public override void PostDraw()
+        {
+            if (HeadContainer.GetInnerHeadData() != null)
+            {
+                Vector3 parentDrawPos = parent.DrawPos;
+                Vector3 loc = new Vector3(parentDrawPos.x + Props.drawOffset.x, Props.altitudeLayer.AltitudeFor() + Props.altitudeSemiOffset, parentDrawPos.z + Props.drawOffset.y);
+                
+                Mesh mesh = MeshPool.GridPlane(Vector2.one);
+                Quaternion quat = Quaternion.identity;
+                if (_uniqueRotation != 0)
+                {
+                    quat = Quaternion.AngleAxis(_uniqueRotation, Vector3.up);
+                }
+
+                Graphics.DrawMesh(mesh, loc, quat, HeadContainer.GetInnerHeadData().Material, 0);
+            }
+        }
+
+        private int _uniqueRotation;
+    }
+}
---
+++ Source/Headstake/CompProperties_HeadDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+
+namespace RWBeheading
+{
+    public class CompProperties_HeadDrawer : CompProperties
+    {
+        public Vector2 drawOffset = Vector2.zero;
+        public AltitudeLayer altitudeLayer = AltitudeLayer.Item;
+        public float altitudeSemiOffset = 0f;
+
+        public IntVec2 rotationRange = IntVec2.Zero;
+        
+        public CompProperties_HeadDrawer()
+        {
+            compClass = typeof(CompHeadDrawer);
+        }
+    }
+}
